6d2e166995dfdf09d0c7d7197596660560ba7ed1
JerryJSpringer <JerryJSpringer@gmail.com>
2020-10-16 17:36:05 -0700

    Now using XNA Vector2 and also added a speed value

--- DefaultEcs/Components/Velocity.cs
+++ DefaultEcs/Components/Velocity.cs
@@ -1,8 +1,9 @@
-﻿using System.Numerics;
+﻿using Microsoft.Xna.Framework;
 
 namespace GameDevIdiotsProject1.DefaultEcs.Components
 {
 	public struct Velocity {
 		public Vector2 Value;
+		public float speed;
 	}
 }
