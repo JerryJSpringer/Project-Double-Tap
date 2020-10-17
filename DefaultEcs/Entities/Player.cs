e1ee145c2116e420591dccf8b599e25cb1dc6ebe
JerryJSpringer <JerryJSpringer@gmail.com>
2020-10-16 17:36:29 -0700

    Updated the player blueprint for changing components

--- DefaultEcs/Entities/Player.cs
+++ DefaultEcs/Entities/Player.cs
@@ -1,21 +1,27 @@
 ﻿using DefaultEcs;
 using GameDevIdiotsProject1.DefaultEcs.Components;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GameDevIdiotsProject1.DefaultEcs.Entities
 {
 	public static class Player
 	{
-		public static void Create(World world)
+		public static void Create(World world, Texture2D texture)
 		{
 			Entity player = world.CreateEntity();
 			player.Set<PlayerInput>(default);
-			player.Set<Velocity>(default);
 			player.Set<Position>(default);
+			player.Set(new Velocity
+			{
+				Value = new Vector2(),
+				speed = .2f
+			});
 			player.Set(new RenderInfo
 			{
-				color = Color.White,
-				sprite = new Rectangle(0, 0, 100, 100)
+				sprite = texture,
+				bounds = new Rectangle(0, 0, 16, 16),
+				color = Color.White
 			});
 		}
 	}
