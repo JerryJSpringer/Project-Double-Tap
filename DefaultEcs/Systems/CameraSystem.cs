3809592725e03d1e80abab583b18560d527306c7
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-09 20:24:40 -0800

    Added a camera system.

---
+++ DefaultEcs/Systems/CameraSystem.cs
@@ -0,0 +1,28 @@
+﻿using DefaultEcs;
+using DefaultEcs.System;
+using GameDevIdiotsProject1.DefaultEcs.Components;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System.Numerics;
+
+namespace GameDevIdiotsProject1.DefaultEcs.Systems
+{
+	public sealed class CameraSystem : AEntitySystem<float>
+	{
+		private readonly OrthographicCamera _camera;
+
+		public CameraSystem(World world, OrthographicCamera camera) :
+			base(world.GetEntities()
+				.With<PlayerInput>()
+				.With<RenderInfo>()
+				.AsSet())		
+		{
+			_camera = camera;
+		}
+
+		protected override void Update(float state, in Entity entity)
+		{
+			_camera.LookAt(entity.Get<Position>().Value);
+		}
+	}
+}
