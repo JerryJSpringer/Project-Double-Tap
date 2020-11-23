e9add2b6d50827e5bd93a5c918ef4e23f5aa64c7
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-23 06:37:57 -0800

    Added component for collision.

---
+++ DefaultEcs/Components/Collision/Collision.cs
@@ -0,0 +1,28 @@
+﻿using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace GameDevIdiotsProject1.DefaultEcs.Components
+{
+	public struct Collision {
+		public ICollisionActor collisionActor;
+	}
+
+	public class CollisionActorEntity : ICollisionActor
+	{
+		public IShapeF Bounds { get; }
+		public Vector2 Velocity;
+		public List<CollisionEventArgs> collisions;
+
+		public CollisionActorEntity(IShapeF bounds)
+		{
+			Bounds = bounds;
+		}
+
+		public void OnCollision(CollisionEventArgs collisionInfo)
+		{
+			collisions.Add(collisionInfo);
+		}
+	}
+}
