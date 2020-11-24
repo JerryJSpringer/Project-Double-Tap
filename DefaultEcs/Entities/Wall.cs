f366111ba0cebca10f6e25da1209cf3783f2b334
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-23 22:29:29 -0800

    Added a simple entity that mimics a wall.

---
+++ DefaultEcs/Entities/Wall.cs
@@ -0,0 +1,37 @@
+﻿using DefaultEcs;
+using GameDevIdiotsProject1.DefaultEcs.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
+
+namespace GameDevIdiotsProject1.DefaultEcs.Entities
+{
+	public static class Wall
+	{
+		public static void Create(World world, CollisionComponent collisionComponent, Texture2D texture)
+		{
+			Entity wall = world.CreateEntity();
+			wall.Set(new Position
+			{
+				Value = new Vector2(40, 40)
+			});
+
+			CollisionActorEntity actor = new CollisionActorEntity(new RectangleF(0, 0, 44, 52));
+			wall.Set(new Collision
+			{
+				collisionActor = actor
+			});
+			collisionComponent.Insert(actor);
+
+			wall.Set(new RenderInfo
+			{
+				sprite = texture,
+				bounds = new Rectangle(40, 0, 44, 52),
+				position = new Vector2(0, 0),
+				color = Color.White,
+				flip = false
+			});
+		}
+	}
+}
