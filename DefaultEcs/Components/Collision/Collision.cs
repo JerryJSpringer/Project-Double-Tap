0bc4a6e090a1d8eb0a17b25733f845c96e3db303
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-23 22:28:48 -0800

    Updated the CollisionActorEntity and added a Collision Type enum.

--- DefaultEcs/Components/Collision/Collision.cs
+++ DefaultEcs/Components/Collision/Collision.cs
@@ -1,28 +1,51 @@
 ﻿using MonoGame.Extended;
 using MonoGame.Extended.Collisions;
-using System.Collections.Generic;
-using System.Numerics;
 
 namespace GameDevIdiotsProject1.DefaultEcs.Components
 {
 	public struct Collision {
 		public ICollisionActor collisionActor;
+		public CollisionActorEntity.Type type;
+
+		public Collision(ICollisionActor aActor, CollisionActorEntity.Type aType) { //?
+			collisionActor = aActor;
+            type = aType;
+        }
 	}
 
 	public class CollisionActorEntity : ICollisionActor
 	{
 		public IShapeF Bounds { get; }
-		public Vector2 Velocity;
-		public List<CollisionEventArgs> collisions;
+		public Type type;
 
 		public CollisionActorEntity(IShapeF bounds)
 		{
 			Bounds = bounds;
 		}
-
+		
 		public void OnCollision(CollisionEventArgs collisionInfo)
 		{
-			collisions.Add(collisionInfo);
+			CollisionActorEntity other = (CollisionActorEntity) collisionInfo.Other;
+		}
+
+		public enum Type {
+			// Take damage
+			DamageCollision = 0,
+
+			// Stop moving
+			NormalCollision = 1,
+
+			// Pickup item
+			ItemPickupCollision = 2,
+
+			// Load new level
+			LoadingZoneCollision = 4,
+
+			// Player collision
+			PlayerCollision = 8,
+
+			// Monster collision
+			MonsterCollision = 16
 		}
 	}
 }
