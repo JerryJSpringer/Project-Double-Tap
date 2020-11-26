11c377aaa35f6823cf8a02ba29d0a5e07657004a
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-26 08:23:08 -0800

    Modified to support the changes in collision.

--- DefaultEcs/Entities/Wall.cs
+++ DefaultEcs/Entities/Wall.cs
@@ -12,20 +12,25 @@ namespace GameDevIdiotsProject1.DefaultEcs.Entities
 		public static void Create(World world, CollisionComponent collisionComponent, Texture2D texture)
 		{
 			Entity wall = world.CreateEntity();
-			wall.Set(new Position {
-				Value = new Vector2(40, 40)
+
+			wall.Set(new Position
+			{
+				Value = new Vector2(100, 100)
 			});
+
 			wall.Set(new RenderInfo {
 				sprite = texture,
 				bounds = new Rectangle(0, 0, 80, 80),
 				color = Color.White,
 				flip = false
 			});
-			CollisionActorEntity actor = new CollisionActorEntity(new RectangleF(0, 0, 80, 80), CollisionActorEntity.Type.MonsterCollision);
+
+			CollisionActorEntity actor = new CollisionActorEntity(
+				new CircleF(new Point2(), 50),
+				CollisionActorEntity.CollisionType.MonsterCollision,
+				ref wall);
 			wall.Set(new Collision(actor));
 			collisionComponent.Insert(actor);
-
-			
 		}
 	}
 }
