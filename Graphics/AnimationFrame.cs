c0f9b6648d37bf3f6610d67848eead626a60b8a5
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-29 10:14:18 -0800

    Removed unused imports.

--- Graphics/AnimationFrame.cs
+++ Graphics/AnimationFrame.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GameDevIdiotsProject1.Graphics {
     public class AnimationFrame {
