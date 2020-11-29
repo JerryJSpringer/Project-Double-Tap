ad281cc8aef53f86afd908b0c9b87573085d7a06
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-29 10:13:59 -0800

    Changed to modify speed instead of the direction vector.

--- Abilities/DodgeRoll.cs
+++ Abilities/DodgeRoll.cs
@@ -20,7 +20,7 @@ namespace GameDevIdiotsProject1.Abilities
 			base.Start(in entity, in world);
 
 			ref Velocity velocity = ref entity.Get<Velocity>();
-			velocity.Value *= SPEED_BOOST;
+			velocity.speed *= SPEED_BOOST;
 		}
 
 		public override void Update(float state, in Entity entity, in World world)
@@ -33,7 +33,7 @@ namespace GameDevIdiotsProject1.Abilities
 			base.End(in entity, in world);
 
 			ref Velocity velocity = ref entity.Get<Velocity>();
-			velocity.Value /= SPEED_BOOST;
+			velocity.speed /= SPEED_BOOST;
 		}
 	}
 }
