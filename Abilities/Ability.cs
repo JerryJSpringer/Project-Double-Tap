686c829c3aff88b95372f003a37414c97e562d47
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-29 10:06:31 -0800

    Framework for all other abilities.

---
+++ Abilities/Ability.cs
@@ -0,0 +1,24 @@
+﻿using DefaultEcs;
+
+namespace GameDevIdiotsProject1.Abilities
+{
+	public abstract class Ability
+	{
+		public float currentTime;
+		public float cooldown;
+		public float duration;
+		public bool movementOverride;
+		public AbilityState abilityState;
+
+		public abstract void Start(in Entity entity, in World world);
+		public abstract void Update(float state, in Entity entity, in World world);
+		public abstract void End(in Entity entity, in World world);
+	}
+
+	public enum AbilityState
+	{
+		AVAILABLE = 0,
+		PERFORMING = 1,
+		COOLDOWN = 2
+	}
+}
---
+++ Abilities/DefaultAbility.cs
@@ -0,0 +1,32 @@
+﻿using DefaultEcs;
+
+namespace GameDevIdiotsProject1.Abilities
+{
+	class DefaultAbility : Ability
+	{
+		public override void Start(in Entity entity, in World world)
+		{
+			currentTime = 0;
+			abilityState = AbilityState.PERFORMING;
+		}
+
+		public override void Update(float state, in Entity entity, in World world)
+		{
+			currentTime += state;
+				
+			if (currentTime > duration)
+			{
+				if (abilityState == AbilityState.COOLDOWN)
+					abilityState = AbilityState.AVAILABLE;
+				else if (abilityState == AbilityState.PERFORMING)
+					End(in entity, in world);
+			}
+		}
+
+		public override void End(in Entity entity, in World world)
+		{
+			currentTime = 0;
+			abilityState = AbilityState.COOLDOWN;
+		}
+	}
+}
