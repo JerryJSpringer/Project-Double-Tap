aaecd07f16701672740849e941474399194a2dd0
Dumplin <avghdev@gmail.com>
2020-12-08 00:26:16 -0800

    Updated Wall Entity to work with scaling

--- DefaultEcs/Entities/Wall.cs
+++ DefaultEcs/Entities/Wall.cs
@@ -9,8 +9,11 @@ namespace GameDevIdiotsProject1.DefaultEcs.Entities
 {
 	public static class Wall
 	{
-		public static void Create(World world, CollisionComponent collisionComponent, Texture2D texture)
+		public static float _scale { get; private set; }
+
+		public static void Create(World world, CollisionComponent collisionComponent, Texture2D texture, float scale = 1f)
 		{
+			_scale = scale;
 			Entity wall = world.CreateEntity();
 
 			wall.Set(new Position
@@ -20,13 +23,14 @@ namespace GameDevIdiotsProject1.DefaultEcs.Entities
 
 			wall.Set(new RenderInfo {
 				sprite = texture,
-				bounds = new Rectangle(0, 0, 80, 80),
+				bounds = new Rectangle(0, 0, 10, 15),
 				color = Color.White,
-				flip = false
+				flip = false,
+				scale = _scale
 			});
 
 			CollisionActorEntity actor = new CollisionActorEntity(
-				new CircleF(new Point2(), 50),
+				new CircleF(new Point2(), 8 * _scale),
 				CollisionActorEntity.CollisionType.MonsterCollision,
 				ref wall);
 			wall.Set(new Collision(actor));
