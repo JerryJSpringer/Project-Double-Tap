be99cf301daa5a02d14079c3b3797619868ec648
Dumplin <avghdev@gmail.com>
2020-12-10 12:28:47 -0800

    Updated DodgeRoll ability to work with dodge-roll animation

--- Abilities/DodgeRoll.cs
+++ Abilities/DodgeRoll.cs
@@ -1,13 +1,15 @@
 ﻿using DefaultEcs;
 using GameDevIdiotsProject1.DefaultEcs.Components;
+using Microsoft.Xna.Framework;
 
 namespace GameDevIdiotsProject1.Abilities
 {
 	class DodgeRoll : DefaultMovementOverrideAbility
 	{
-		private const int SPEED_BOOST = 2;
+		private const float SPEED_BOOST = 1.8f;
 		private const int COOLDOWN = 200;
-		private const int DURATION = 200;
+		private const int DURATION = 400;
+		private const string ANIMATION_KEY = "dodge-roll";
 
 		public DodgeRoll()
 		{
@@ -21,6 +23,14 @@ namespace GameDevIdiotsProject1.Abilities
 
 			ref Velocity velocity = ref entity.Get<Velocity>();
 			velocity.speed *= SPEED_BOOST;
+
+			if (!velocity.Value.Equals(new Vector2(0, 0))) {
+				ref Animate animation = ref entity.Get<Animate>();
+				animation.currentAnimation = ANIMATION_KEY;
+			}
+			else {
+				End(in entity, in world);
+            }
 		}
 
 		public override void Update(float state, in Entity entity, in World world)
@@ -34,6 +44,9 @@ namespace GameDevIdiotsProject1.Abilities
 
 			ref Velocity velocity = ref entity.Get<Velocity>();
 			velocity.speed /= SPEED_BOOST;
+
+			ref Animate animation = ref entity.Get<Animate>();
+			animation.currentAnimation = "walk";
 		}
 	}
 }
