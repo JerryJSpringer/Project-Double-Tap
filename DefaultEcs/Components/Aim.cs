89e13afb5f1fbd0c757a99558df74b54ff0f8694
JerryJSpringer <JerryJSpringer@gmail.com>
2020-12-18 09:41:36 -0800

    Added the ability to unwrap mouse position into a world position value.

---
+++ DefaultEcs/Components/Aim.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.Xna.Framework;
+
+namespace GameDevIdiotsProject1.DefaultEcs.Components
+{
+	public struct Aim
+	{
+		public Vector2 Value;
+	}
+}
--- DefaultEcs/Systems/CameraSystem.cs
+++ DefaultEcs/Systems/CameraSystem.cs
@@ -1,10 +1,10 @@
 ﻿using DefaultEcs;
 using DefaultEcs.System;
 using GameDevIdiotsProject1.DefaultEcs.Components;
+using GameDevIdiotsProject1.Util;
 using Microsoft.Xna.Framework;
 using MonoGame.Extended;
 using MonoGame.Extended.Tiled;
-using System;
 
 namespace GameDevIdiotsProject1.DefaultEcs.Systems
 {
@@ -14,8 +14,9 @@ namespace GameDevIdiotsProject1.DefaultEcs.Systems
 		private readonly int _center;
         private readonly OrthographicCamera _camera;
 		private readonly TiledMap _tiledMap;
+		private readonly GameWindow _window;
 
-		public CameraSystem(World world, OrthographicCamera camera, TiledMap tiledMap, int gamesize) :
+		public CameraSystem(World world, OrthographicCamera camera, TiledMap tiledMap, GameWindow window, int gamesize) :
 			base(world.GetEntities()
 				.With<PlayerInput>()
 				.With<RenderInfo>()
@@ -23,22 +24,40 @@ namespace GameDevIdiotsProject1.DefaultEcs.Systems
 		{
 			_camera = camera;
 			_tiledMap = tiledMap;
+			_window = window;
 			_center = gamesize / 2;
 		}
 
 		protected override void Update(float state, in Entity entity)
 		{
-			Vector2 pos = new Vector2();
+			Vector2 charPosition = entity.Get<Position>().Value;
 
-			pos.X = (entity.Get<Position>().Value.X <= _center) ? _center
-				: (entity.Get<Position>().Value.X >= _tiledMap.WidthInPixels - _center) ? _tiledMap.WidthInPixels - _center
-				: entity.Get<Position>().Value.X;
+			Vector2 cameraPosition = new Vector2
+			{
+				X = (charPosition.X <= _center) ? _center
+				: (charPosition.X >= _tiledMap.WidthInPixels - _center) ? _tiledMap.WidthInPixels - _center
+				: charPosition.X,
 
-			pos.Y = (entity.Get<Position>().Value.Y <= _center) ? _center
-				: (entity.Get<Position>().Value.Y >= _tiledMap.HeightInPixels - _center) ? _tiledMap.HeightInPixels - _center
-				: entity.Get<Position>().Value.Y;
+				Y = (charPosition.Y <= _center) ? _center
+				: (charPosition.Y >= _tiledMap.HeightInPixels - _center) ? _tiledMap.HeightInPixels - _center
+				: charPosition.Y
+			};
 
-			_camera.LookAt(pos);
+			int width = _window.ClientBounds.Width / 2;
+			int height = _window.ClientBounds.Height / 2;
+
+			CameraFactory.Update(new Vector2
+			{
+				X = (charPosition.X <= width) ? charPosition.X
+				: (charPosition.X >= _tiledMap.WidthInPixels - width) ? charPosition.X - _tiledMap.WidthInPixels + 2 * width
+				: width,
+
+				Y = (charPosition.Y <= height) ? charPosition.Y
+				: (charPosition.Y >= _tiledMap.HeightInPixels - height) ? charPosition.Y - _tiledMap.HeightInPixels + 2 * height
+				: height
+			});
+
+			_camera.LookAt(cameraPosition);
 			
 		}
 	}
