7e4b776594f7e0d8e384f20c91ef0d60503e6a16
JerryJSpringer <JerryJSpringer@gmail.com>
2020-12-18 09:53:25 -0800

    Added a double tap ability mechanic with a double tap dash as an example.

---
+++ Abilities/Dash.cs
@@ -0,0 +1,47 @@
+﻿using DefaultEcs;
+using GameDevIdiotsProject1.DefaultEcs.Components;
+using GameDevIdiotsProject1.Util;
+using Microsoft.Xna.Framework;
+
+namespace GameDevIdiotsProject1.Abilities
+{
+	class Dash : DoubleTapAbility
+	{
+		private const float COOLDOWN = 500;
+		private const float DURATION = 100;
+		private const float DASH_DISTANCE = 30;
+		private float _distance;
+		private MovementDirection _direction;
+
+		public Dash(Command command, MovementDirection direction) : base (command)
+		{
+			_direction = direction;
+			_distance = DASH_DISTANCE;
+			cooldown = COOLDOWN;
+			duration = DURATION;
+		}
+
+		protected override void Action(in Entity entity)
+		{
+			base.Action(in entity);
+
+			ref Vector2 position = ref entity.Get<Position>().Value;
+
+			switch (_direction)
+			{
+				case MovementDirection.LEFT:
+					position.X += -_distance;
+					break;
+				case MovementDirection.RIGHT:
+					position.X += _distance;
+					break;
+				case MovementDirection.UP:
+					position.Y += -_distance;
+					break;
+				case MovementDirection.DOWN:
+					position.Y += _distance;
+					break;
+			}
+		}
+	}
+}
---
+++ Abilities/DoubleTapAbility.cs
@@ -0,0 +1,49 @@
+﻿using DefaultEcs;
+using GameDevIdiotsProject1.Util;
+
+namespace GameDevIdiotsProject1.Abilities
+{
+	abstract class DoubleTapAbility : DefaultAbility
+	{
+		private const float DEFAULT_MAX_WINDOW = 200;
+		private float maxWindow;
+		private bool _released;
+		
+		public DoubleTapAbility(Command command) : base (command)
+		{
+			maxWindow = DEFAULT_MAX_WINDOW;
+		}
+
+		public override void Start(in Entity entity)
+		{
+			if (state == AbilityState.AVAILABLE && currentTime < maxWindow)
+			{
+				if (_released)
+					Action(in entity);
+			} 
+			else if (currentTime > maxWindow)
+			{
+				_released = false;
+				currentTime = 0;
+			}
+		}
+
+		protected virtual void Action(in Entity entity)
+		{
+			base.Start(in entity);
+		}
+
+		public override void Update(float delta, in Entity entity)
+		{
+			base.Update(delta, entity);
+			if (!command.IsPressed())
+				_released = true;
+		}
+
+		public override void End(in Entity entity)
+		{
+			base.End(entity);
+			_released = false;
+		}
+	}
+}
