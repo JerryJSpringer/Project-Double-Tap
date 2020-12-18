7443a3771d9a3919f205a48a60905261cbe240fe
JerryJSpringer <JerryJSpringer@gmail.com>
2020-12-18 09:55:04 -0800

    Added a shooting mechanic in gun to show off new aiming features.

---
+++ Abilities/Gun.cs
@@ -0,0 +1,31 @@
+﻿using DefaultEcs;
+using GameDevIdiotsProject1.DefaultEcs.Components;
+using GameDevIdiotsProject1.DefaultEcs.Entities;
+using GameDevIdiotsProject1.Util;
+
+namespace GameDevIdiotsProject1.Abilities
+{
+	class Gun : DefaultAbility
+	{
+		private const int COOLDOWN = 200;
+		private const int DURATION = 0;
+		private const float BULLET_SPEED = .5f;
+
+		public Gun(Command command) : base(command)
+		{
+			cooldown = COOLDOWN;
+			duration = DURATION;
+
+			types.AddRange(new AbilityType[]
+			{
+				AbilityType.OVERRIDABLE
+			});
+		}
+
+		public override void Start(in Entity entity)
+		{
+			base.Start(in entity);
+			BulletFactory.Create(entity.Get<Position>().Value, entity.Get<Aim>().Value, BULLET_SPEED);
+		}
+	}
+}
