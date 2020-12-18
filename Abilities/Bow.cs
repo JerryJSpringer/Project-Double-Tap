67948a20d2221d20615decea40c652912b8b346e
JerryJSpringer <JerryJSpringer@gmail.com>
2020-12-18 09:52:02 -0800

    Added a charge ability mechanic with an example shown in bow.

---
+++ Abilities/Bow.cs
@@ -0,0 +1,34 @@
+﻿using DefaultEcs;
+using GameDevIdiotsProject1.DefaultEcs.Components;
+using GameDevIdiotsProject1.DefaultEcs.Entities;
+using GameDevIdiotsProject1.Util;
+
+namespace GameDevIdiotsProject1.Abilities
+{
+	class Bow : ChargeAbility
+	{
+		private const int COOLDOWN = 200;
+		private const int MAX_CHARGE = 500;
+		private const int MAX_MULTI = 4;
+		private const float BULLET_SPEED = .25f;
+
+		public Bow(Command command) : base(command)
+		{
+			cooldown = COOLDOWN;
+			duration = MAX_CHARGE;
+			maxMulti = MAX_MULTI;
+
+			types.AddRange(new AbilityType[]
+			{
+				AbilityType.CHARGE,
+				AbilityType.OVERRIDABLE
+			});
+		}
+
+		public override void End(in Entity entity)
+		{
+			base.End(in entity);
+			BulletFactory.Create(entity.Get<Position>().Value, entity.Get<Aim>().Value, BULLET_SPEED * multi);
+		}
+	}
+}
---
+++ Abilities/ChargeAbility.cs
@@ -0,0 +1,45 @@
+﻿using DefaultEcs;
+using GameDevIdiotsProject1.Util;
+
+namespace GameDevIdiotsProject1.Abilities
+{
+	abstract class ChargeAbility : Ability
+	{
+		protected float maxMulti;
+		protected float multi;
+
+		public ChargeAbility(Command command) : base (command)
+		{
+		}
+
+		public override void Start(in Entity entity)
+		{
+			state = AbilityState.CHARGING;
+			currentTime = 0;
+		}
+
+		public override void Update(float delta, in Entity entity)
+		{
+			currentTime += delta;
+			if (state == AbilityState.COOLDOWN && currentTime > cooldown)
+				state = AbilityState.AVAILABLE;
+			else if (state == AbilityState.CHARGING && !command.IsPressed())
+				End(in entity);
+		}
+
+		public override void End(in Entity entity)
+		{
+			state = AbilityState.COOLDOWN;
+			if (currentTime > duration)
+			{
+				multi = maxMulti;
+			}
+			else
+			{
+				multi = (currentTime / duration) * maxMulti;
+				if (multi < 1)
+					multi = 1;
+			}
+		}
+	}
+}
